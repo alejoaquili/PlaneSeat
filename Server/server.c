826fb26d344d52e1150fa996fbfa24193ac4db05
Francisco Sanguineti <fsanguineti@itba.edu.ar>
2018-06-21 02:58:05 -0300

    server3

--- Server/server.c
+++ Server/server.c
@@ -8,57 +8,55 @@
 #include <netinet/in.h>
 
 #define CONNECTIONS_QTY 2
+#define SEM_NAME "conectionsSemaphore"
 #define PORT 7200
 
-void sigchld_handler(int sig);
+void sigChildHandler(int sig);
 
+static int listenFd;
 static sem_t * semaphore;
 
 int main(int argc , char *argv[])
 {
-    signal(SIGCHLD, sigchldHandler);
-    signal(SIGKILL, sigendHandler);
-    signal(SIGTERM, sigendHandler);
-    signal(SIGKICK, sigendHandler);
-    semaphore = sem_open("santiputo3", O_CREAT|O_EXCL, 0777, CONNECTIONS_QTY);
+    signal(SIGCHLD, sigChildHandler);
+    signal(SIGKILL, sigEndHandler);
+    signal(SIGTERM, sigEndHandler);
+    signal(SIGQUIT, sigEndHandler);
+    semaphore = sem_open(SEM_NAME, O_CREAT|O_EXCL, 0777, CONNECTIONS_QTY);
 
-    int listenFd = openSocket();
-    checkFail(listenFd, "Error en listener\n");
+    listenFd = openSocket();
+    checkFail(listenFd, "Error in listener\n");
     
     bindToPort(listenFd, PORT);
-    checkFail(listen(listenFd, 10) == -1, "No es posible escuchar en ese puerto\n");
-    printf("Enlazado al puerto");
+    checkFail(listen(listenFd, 10) == -1, "Unable to linten in this port\n");
+    printf("Port binded");
     
     while(1)
     {
-        struct sockaddr_storage client;
-        unsigned int addressSize = sizeof(client);
-        printf("Esperando al cliente");
-        
-        int connectFd = accept(listenFd, (struct sockaddr *)&client, &addressSize);
-        if(connectFd == -1)
-        {
-            printf("se cago, hay q handelear la falla de Conexion de servidor");
-        }
+        int connectFd = acceptClientSocket(listenFd);
 
         sem_wait(semaphore);
-        if(fork() == 0) 
+        switch(fork()) 
         {
-            close(listenFd);
-            sem_close(semaphore);
-            printf("Conexion aceptada\n");
-            execlp(CONECTION_HANDLER, " ", ((char *)NULL));
+            case 0:
+                close(listenFd);
+                sem_close(semaphore);
+                printf("Conexion aceptada\n");
+                execlp(CONECTION_HANDLER, " ", ((char *)NULL));
 
-			fail("exec() Failed");
-            close(connectFd);
-            exit(0);
+                fail("exec() Failed");
+                close(connectFd);
+                exit(0);
+            case -1:
+                fail("Conection fork fail");
+            default:
+                close(connectFd); 
         }
-        close(connectFd); 
     }
     return 0;
 }
 
-void sigchldHandler(int sig)
+void sigChildHandler(int sig)
 {
     sem_post(semaphore);
     while(waitpid(-1, 0, WNOHANG) > 0);
@@ -66,10 +64,9 @@ void sigchldHandler(int sig)
 }
 
 
-void sigendHandler(int sig)
+void sigEndHandler(int sig)
 {
-    sem_unlink(shm->semName);
-    close()
-    while(waitpid(-1, 0, WNOHANG) > 0);
+    sem_unlink(SEM_NAME);
+    close(listenFd);
     return;
 }
