fd2cbc6727d52168e90fb32a21a23aa83b2900c7
santiagobassani96 <santiago.bassani96@gmail.com>
2018-06-22 19:28:45 -0300

    serialization complete

--- Utils/serialization.c
+++ Utils/serialization.c
@@ -3,7 +3,7 @@
 #include <string.h>
 // supported data types
 // arrays must be the ones declare in here no native array will be supported
-typedef enum type {Integer, Double, String, Array, Object} Type;
+typedef enum type {Integer, Double, String, Array, Object, Json, Undefined} Type;
 
 typedef struct arrayNodeCDT{
     struct arrayNodeCDT * next;
@@ -58,13 +58,14 @@ int getArraySize(arrayADT array);
 int addNodeToArray(arrayADT array, arrayNodeADT node);
 void* getValueInArray(arrayADT array ,int index);
 Type getTypeFromIndex(arrayADT array, int index);
+int deleteNodeFromArray(arrayADT array, int index);
 
 void freeArray(arrayADT array);
 static void freeArrayNode(arrayNodeADT node);
 static void freeList(arrayNodeADT node);
 static void* searchForValue(arrayNodeADT node, int index);
 static arrayNodeADT getNodeAt(arrayNodeADT node, int index);
-
+static arrayNodeADT removeNode(arrayNodeADT node, int index);
 char* arrayNodeToString(arrayNodeADT node);
 char * toString(void * value, Type type);
 
@@ -84,6 +85,23 @@ void getLastFromBuffer(bufferADT buffer, void* destination, int size);
 void freeBuffer(bufferADT buffer);
 static int addSpaceToBuffer(bufferADT buffer, int size);
 //------------------------------- end -----------------------------------------
+
+//-------------------- Beginning of Object contract ---------------------------
+objectADT newObject(char* key, void* value, Type type);
+void* getObjectValue(objectADT object);
+char* getObjectKey(objectADT object);
+Type getObjectType(objectADT object);
+char* objectToString(objectADT object);
+void freeObject(objectADT object);
+//------------------------ End of Object Contract -----------------------------
+
+//-------------------- Beginning of Json contract -----------------------------
+
+jsonADT newJsonObject();
+int addObject(jsonADT json, objectADT object);
+int removeObject(jsonADT json, char* key);
+char * jsonToString(jsonADT json);
+//------------------------ End of Json Contract -------------------------------
 
 // Array strucutre methods
 
@@ -115,6 +133,15 @@ int addNodeToArray(arrayADT array, arrayNodeADT node)
     return array->size;
 }
 
+int deleteNodeFromArray(arrayADT array, int index)
+{
+    if(array == NULL || index < 0 || index >= array->size)
+        return -1;
+    
+    array->first =  removeNode(array->first, index);
+    return 1;
+}
+
 void* getValueInArray(arrayADT array ,int index)
 {
     if(array == NULL)
@@ -126,9 +153,11 @@ void* getValueInArray(arrayADT array ,int index)
 Type getTypeFromIndex(arrayADT array, int index)
 {
     if(array == NULL || index < 0)
-        return NULL;
+        return Undefined;
     
     arrayNodeADT node = getNodeAt(array->first, index);
+    if(node == NULL)
+        return Undefined;
     return node->type;
 }
 
@@ -177,6 +206,17 @@ static arrayNodeADT getNodeAt(arrayNodeADT node, int index)
     return getNodeAt(node->next, index-1);
 }
 
+static arrayNodeADT removeNode(arrayNodeADT node, int index)
+{
+    if(node == NULL)
+        return node;
+    if(index == 0)
+        return node->next;
+    
+    node->next = removeNode(node->next, index-1);
+    return node;
+}
+
 
 // serialization methods
 char* arrayNodeToString(arrayNodeADT node)
@@ -198,6 +238,13 @@ char * toString(void * value, Type type)
             return stringToString((char*)value);
         case Array:
             return arrayToString((arrayADT) value);
+        case Object:
+            return objectToString((objectADT) value);
+        case Json:
+            return jsonToString((jsonADT) value);
+        case Undefined:
+            printf("Undefine!\n");
+            break;
     }
 
     printf("not implemented yet...\n");
@@ -223,18 +270,24 @@ char* intToString(int number)
 char* doubleToString(double number)
 {
     // this will get you the number of chars needed for the number
-    int length = snprintf(NULL, 0, "%g", number);
+    int length = snprintf(NULL, 0, "%.2f", number);
     //the +1 y for null terminated string
     char* numberString = calloc(length+1, sizeof(char));
-    snprintf(numberString, length+1, "%g", number);
+    snprintf(numberString, length+1, "%.2f", number);
     return numberString;
 }
 
 char* stringToString( char* string)
-{
-    char* newString = calloc(strlen(string)+1, sizeof(char));
-    memcpy(newString, string, strlen(string));
-    return string;
+{   
+    char* quoteMark = "\"";
+    char* newString = calloc(strlen(string)+3, sizeof(char));
+    bufferADT buffer = newBuffer();
+    addToBuffer(buffer, quoteMark,1);
+    addToBuffer(buffer, string, strlen(string));
+    addToBuffer(buffer, quoteMark, 1);
+    getLastFromBuffer(buffer, newString, getBufferSize(buffer));
+    freeBuffer(buffer);
+    return newString;
 }
 
 
@@ -261,6 +314,28 @@ char* arrayToString(arrayADT array)
     freeBuffer(buffer);
     return composedArrayString;    
 }
+
+char* objectToString(objectADT object)
+{
+    char* semicolon = ":";
+    char* quoteMark = "\"";
+    int size = strlen(getObjectKey(object));
+    char * valueRepresentation = toString(getObjectValue(object), getObjectType(object));
+    size += strlen(valueRepresentation) + 4;
+    bufferADT buffer = newBuffer();
+    addToBuffer(buffer, quoteMark, 1);
+    addToBuffer(buffer,getObjectKey(object),strlen(getObjectKey(object)));
+    addToBuffer(buffer, quoteMark, 1);
+    addToBuffer(buffer, semicolon, 1);
+    addToBuffer(buffer,valueRepresentation , strlen(valueRepresentation));
+
+    char* objectRepresentation = calloc(size, sizeof(char));
+    getLastFromBuffer(buffer, objectRepresentation, getBufferSize(buffer));
+    freeBuffer(buffer);
+    return objectRepresentation;
+
+}
+
 
 //------------------- End of primitives string methods ------------------------
 
@@ -323,6 +398,174 @@ void freeBuffer(bufferADT buffer)
 
 // --------------------------- end of buffer ----------------------------------
 
+//------------------------ Beginning of object methods ------------------------
+
+// NOTICE : the arrayADT in json should be changed as it comes  
+//  with a lot of overhead for nothing.
+
+objectADT newObject(char* key, void* value, Type type)
+{
+    objectADT new = calloc(1, sizeof(objectCDT));
+    new->key = key;
+    new->type = type;
+    new->value = value;
+    return new;
+}
+
+void* getObjectValue(objectADT object)
+{
+    if(object == NULL)
+        return NULL;
+
+    return object->value;
+}
+
+char* getObjectKey(objectADT object)
+{
+    if(object == NULL)
+        return "none";
+    
+    return object->key;
+}
+
+Type getObjectType(objectADT object)
+{
+    if(object == NULL)
+        return Undefined;
+    return object->type;
+}
+
+void freeObject(objectADT object)
+{
+    free(object->value);
+    free(object->key);
+    free(object);
+}
+
+
+jsonADT newJsonObject()
+{
+    jsonADT new = calloc(1, sizeof(jsonCDT));
+    new->nodes = newArray();
+    new->size = 0;
+    return new;
+}
+
+int freeJson(jsonADT json)
+{
+    if(json == NULL)
+        return -1;
+    freeArray(json->nodes);
+    free(json);
+    return 1;
+}
+
+int addObject(jsonADT json, objectADT object)
+{
+    if(json == NULL || object == NULL)
+        return -1;
+
+    arrayADT array  = json->nodes;
+    arrayNodeADT node = newArrayNode(object, Object);
+    char* aux =  arrayNodeToString(node);
+    int result = addNodeToArray(array, node);
+    if(result != 0)
+        json->size++;
+    return result;
+}
+
+int removeObject(jsonADT json, char* key)
+{
+    if(json == NULL || key == NULL)
+        return -1;
+
+    arrayADT array = json->nodes;
+    for(int i = 0; i < getArraySize(array); i++)
+    {
+        objectADT object = getValueInArray(array, i);
+        char* currentKey = getObjectKey(object);
+        if(strcmp(currentKey, key) == 0)
+        {
+            deleteNodeFromArray(array, i);
+            return 1;
+        }
+    }
+    return 0;
+}
+
+char * jsonToString(jsonADT json)
+{
+    if(json == NULL)
+        return NULL;
+    char* opening = "{";
+    char* closing = "}";
+    char* comma = ",";
+    bufferADT buffer = newBuffer();
+    addToBuffer(buffer, opening, 1);
+    for(int i = json->size-1 ; i >= 0; i--)
+    {
+        objectADT object = getValueInArray(json->nodes, i);
+        char* objectRepresentation = toString(object, Object);
+        
+        int size = strlen(objectRepresentation);
+        addToBuffer(buffer,objectRepresentation, size);
+        if(i  != 0)
+            addToBuffer(buffer, comma,1);
+    }
+    addToBuffer(buffer, closing,1);
+    char* jsonRepresentation = calloc(getBufferSize(buffer)+1, sizeof(char));
+    getLastFromBuffer(buffer, jsonRepresentation, getBufferSize(buffer));
+    freeBuffer(buffer);
+    return jsonRepresentation;
+}
+
+
+//--------------------------- End of Object methods ---------------------------
+
+
+
+// ---------------------- Begginning of deserialization mathods ---------------
+
+
+// ------------------------ Deserialization of primitives ---------------------
+
+int valueOfInt(char* string)
+{
+    int value = atoi(string);
+    return value;
+}
+
+double valueofDouble(char* string)
+{
+    double value = strtod(string, NULL);
+    return value;
+}
+
+char* valueOfString(char* string)
+{
+    int size = strlen(string);
+    char* copyString = calloc(size, sizeof(char));
+    memcpy(copyString, string, size );
+    return copyString;
+}
+
+
+
+// ------------------------ End deserialization of primitives -----------------
+
+
+
+// ---------------------- End of deserialization mathods ----------------------
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -338,7 +581,7 @@ int test_doubleToString(void)
 {
     double x = 450;
     char* number = doubleToString(x);
-    return strcmp(number, "450") == 0;
+    return strcmp(number, "450.00") == 0;
 }
 
 int testNodeArray(void)
@@ -367,7 +610,7 @@ int testArrayToString(void)
     addNodeToArray(array, node2);
     addNodeToArray(array, node3);
     char* string = arrayToString(array);
-    char* expectedResult = "[poo,pi,paw]";
+    char* expectedResult = "[\"poo\",\"pi\",\"paw\"]";
     return strcmp(string,expectedResult ) == 0;
 }
 
@@ -398,11 +641,40 @@ int testArrayOfArraytoString(void)
 
     addNodeToArray(array, subjectsNode);
     char* string = arrayToString(array);
-    char* expectedResult = "[poo,pi,paw]";
-    printf("the result was ... %s\n", string );
+    char* expectedResult = "[\"santi\",21,[\"poo\",\"pi\",\"paw\"]]";
     return strcmp(string,expectedResult ) == 0;
 }
 
+
+int testObjectToString()
+{
+    objectADT object = newObject("name", "Santi", String);
+    char* objectRepresentation = objectToString(object);
+    char* expectedResult = "\"name\":\"Santi\"";
+
+    return strcmp(expectedResult, objectRepresentation) == 0;
+}
+
+int testJsonToString()
+{
+    jsonADT json = newJsonObject();
+    objectADT object =  newObject("name", "santiago",String);
+    addObject(json, object);
+
+    char* string = jsonToString(json);
+    char* expectedResult = "{\"name\":\"santiago\"}";
+
+    return strcmp(string, expectedResult) == 0;
+}
+
+
+int testValueOfDouble()
+{
+    char* value = "450.00";
+    double expectedResult = 450.00;
+    double number = valueofDouble(value);
+    return number - expectedResult == 0;
+}
 
 int testBuffer()
 {
@@ -425,5 +697,11 @@ int main(void)
     printf(" the result was %s\n",(testBuffer() == 0)?"False" : "True" );
     printf(" the result was %s\n",(testArrayToString() == 0)?"False" : "True" );
     printf(" the result was %s\n",(testArrayOfArraytoString() == 0)?"False" : "True" );
-    printf("DONE!\n");
+    printf(" the result was %s\n",(testObjectToString() == 0)?"False" : "True" );
+    printf(" the result was %s\n",(testJsonToString() == 0)?"False" : "True" );
+    printf("DONE! serialization tests\n");
+    printf("\n");
+    printf("\n");
+    printf("Going to test deserializatino...\n");
+    printf(" the result was %s\n",(testValueOfDouble() == 0)?"False" : "True" );
 }
