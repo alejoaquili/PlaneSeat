d919ff96de38ef13e293c34ff59717a03f832919
alejoaquili <aaquili@itba.edu.ar>
2018-06-24 17:09:16 -0300

    scanf

--- Client/commands.c
+++ Client/commands.c
@@ -25,7 +25,7 @@ int applyToGetFlights(int clientSocketFd)
 
     char * string = readStringToDeserialize(clientSocketFd);
     flights = deserializeToFlights(string, &qty);
-
+    printf("QTY:%d\n", qty);
 	printf("This is the list of flights: \n");
 	for(int i = 0; i < qty; i++)
 		printf("\tFlight Number: %s, Origin: %s, Destination: %s\n", flights[i].flightNumber, flights[i].origin, flights[i].destination);
