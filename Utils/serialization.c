1bbde6e39fc88f47a589c91b92309742ea598625
santiagobassani96 <santiago.bassani96@gmail.com>
2018-06-24 17:38:44 -0300

    fix append array

--- Utils/serialization.c
+++ Utils/serialization.c
@@ -119,7 +119,8 @@ static void addNodeToEndOfArray(arrayADT array, arrayNodeADT node)
     if(array == NULL)
         return;
     array->first = appendNodeToArray(array->first, node);
-    array->size++;
+    if(node == NULL)
+        array->size++;
 }
 
 static arrayNodeADT appendNodeToArray(arrayNodeADT current, arrayNodeADT node)
