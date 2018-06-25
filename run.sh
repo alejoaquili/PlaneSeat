d05bd02f4e030f47e7c8ffadef16cd216f96256f
santiagobassani96 <santiago.bassani96@gmail.com>
2018-06-24 23:14:41 -0300

    run

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-
+xterm -title "App 1" -hold -e ./Server/server.out   &
+xterm -title "App 2" -hold -e ./Client/client.out   &
+xterm -title "App 3" -hold -e ./Client/client.out   &
+xterm -title "App 4" -hold -e ./Client/client.out   &
