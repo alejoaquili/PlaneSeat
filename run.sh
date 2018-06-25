24fb8172e862491441e72099b051fab2df4f6baa
santiagobassani96 <santiago.bassani96@gmail.com>
2018-06-24 23:47:28 -0300

    Entrega

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-xterm -title "App 1" -hold -e ./Server/server.out   &
-xterm -title "App 2" -hold -e ./Client/client.out   &
-xterm -title "App 3" -hold -e ./Client/client.out   &
-xterm -title "App 4" -hold -e ./Client/client.out   &
+#x-terminal-emulator -title "App 1" -hold -e ./Server/server.out   &
+#x-terminal-emulator -title "App 2" -hold -e ./Client/client.out   &
+#x-terminal-emulator -title "App 3" -hold -e ./Client/client.out   &
+#x-terminal-emulator -title "App 4" -hold -e ./Client/client.out   &
+osascript -e 'tell app "Terminal" to do script "/Server/server.out"'
+osascript -e 'tell app "Terminal" to do script "./Client/client.out"'
+osascript -e 'tell app "Terminal" to do script "./Client/client.out"'
+osascript -e 'tell app "Terminal" to do script "./Client/client.out"'
