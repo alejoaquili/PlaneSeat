ea1e3ba7e1b85c10e778fa915384e405a456656f
alejoaquili <aaquili@itba.edu.ar>
2018-06-24 23:51:07 -0300

    Entrega

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#x-terminal-emulator -title "App 1" -hold -e ./Server/server.out   &
-#x-terminal-emulator -title "App 2" -hold -e ./Client/client.out   &
-#x-terminal-emulator -title "App 3" -hold -e ./Client/client.out   &
-#x-terminal-emulator -title "App 4" -hold -e ./Client/client.out   &
-osascript -e 'tell app "Terminal" to do script "/Server/server.out"'
-osascript -e 'tell app "Terminal" to do script "./Client/client.out"'
-osascript -e 'tell app "Terminal" to do script "./Client/client.out"'
-osascript -e 'tell app "Terminal" to do script "./Client/client.out"'
